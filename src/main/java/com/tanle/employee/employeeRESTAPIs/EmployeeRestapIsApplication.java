0946d5eaccc87ed82e89cef29e22d45af9eeb770
Le Nhut Tan <121606134+lenhuttanprograming@users.noreply.github.com>
2024-05-27 14:26:44 -0400

    Init spring boot project

---
+++ src/main/java/com/tanle/employee/employeeRESTAPIs/EmployeeRestapIsApplication.java
@@ -0,0 +1,13 @@
+package com.tanle.employee.employeeRESTAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestapIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestapIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanle/employee/employeeRESTAPIs/EmployeeRestapIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanle.employee.employeeRESTAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestapIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
