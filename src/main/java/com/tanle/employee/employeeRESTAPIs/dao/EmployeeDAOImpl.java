02424f58983260cf934bc0dc255da32b9f03fc57
Le Nhut Tan <121606134+lenhuttanprograming@users.noreply.github.com>
2024-06-03 11:53:33 -0400

    Add CUD operations

--- src/main/java/com/tanle/employee/employeeRESTAPIs/dao/EmployeeDAOImpl.java
+++ src/main/java/com/tanle/employee/employeeRESTAPIs/dao/EmployeeDAOImpl.java
@@ -31,4 +31,20 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         // return result
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        return entityManager.find(Employee.class, theId);
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return entityManager.merge(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
+    }
 }
--- src/main/java/com/tanle/employee/employeeRESTAPIs/rest/EmployeeRestController.java
+++ src/main/java/com/tanle/employee/employeeRESTAPIs/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.tanle.employee.employeeRESTAPIs.dao.EmployeeDAO;
 import com.tanle.employee.employeeRESTAPIs.entity.Employee;
 import com.tanle.employee.employeeRESTAPIs.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,47 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{theId}")
+    public Employee findById(@PathVariable int theId){
+        Employee employee = employeeService.findById(theId);
+
+        if(employee == null){
+            throw new RuntimeException("The id is out of bound" + theId);
+        }
+
+        return employeeService.findById(theId);
+    }
+
+    @PostMapping("/employees")
+    public Employee save(@RequestBody Employee theEmployee){
+
+        // Set id to 0 just in case the json data has been set an id value
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return  dbEmployee;
+    }
+
+    @PutMapping("employees")
+    public Employee update(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @DeleteMapping("employees/{employeeId}")
+    public String delete(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("The id is out of bound " + employeeId);
+        }
+
+        employeeService.delete(employeeId);
+
+        return "The employee with id " + employeeId + " has been deleted!";
     }
 }
